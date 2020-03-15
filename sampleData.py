8695b223f8979ce3ba7f339c50e7a308e63aae01
kartic <cleverkriss@gmail.com>
2020-03-15 18:56:40 +0000

    task select only for current date

---
+++ sampleData.py
@@ -0,0 +1,29 @@
+from calendar import monthrange
+year = 2020
+data = []
+# for i in range(1,13):
+#     days =  monthrange(year, i)[1]
+#     month = []
+#     for k in range(1, days+1):
+#         if(days == 31):
+#             month.append(1)
+#         else:
+#             month.append(0)
+#     data.append(month)
+
+
+print(data)
+
+for i in range(1, 32):
+    row = []
+    for j in range(1,13):
+        tot = monthrange(year, j)[1]
+        if(i <= tot):
+            if( tot == 31):
+                row.append(1)
+            else:
+                row.append(0)
+    data.append(row)
+print(data)
+print("rows: ", len(data))
+print("columns: ", len(data[0]))
