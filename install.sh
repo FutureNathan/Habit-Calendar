493cadbf72903ba6be09685f73e36f75cbf28d25
kartic <cleverkriss@gmail.com>
2020-03-23 08:36:32 -0400

    install script

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
-# apt-get update 
-# apt-get upgrade -y
-# apt-get install python3 python3-pip git -y
+apt-get update 
+apt-get upgrade -y
+apt-get install python3 python3-pip git -y
 
-# git clone https://github.com/FutureNathan/Habit-Calendar.git
+git clone https://github.com/FutureNathan/Habit-Calendar.git
 
-# cd Habit-Calendar
-# pip3 install -r requirements.txt
+pip3 install -r Habit-Calendar/requirements.txt
 
 cp Habit-Calendar/Habit-Calendar-Server.service /etc/systemd/system/Habit-Calendar-Servers.service
 cp -R Habit-Calendar /var/Habit-Calendar
