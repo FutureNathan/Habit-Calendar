f574af1179fe53ee554324a08b116eca862d54e1
kartic <cleverkriss@gmail.com>
2020-03-23 09:02:20 -0400

    db directory fix

--- calendarDatabase.py
+++ calendarDatabase.py
@@ -3,7 +3,7 @@ import time
 import datetime
 from calendar import monthrange
 
-database_dir = '/home/pi/Habit-Calendar/habit-calendar.db'
+database_dir = '/var/Habit-Calendar/habit-calendar.db'
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
             "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
         ]
