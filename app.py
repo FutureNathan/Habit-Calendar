1844de9c289dc3dea0f4945d3505eef9765210cb
habit <habit-calendar@habit>
2020-03-26 11:12:50 -0400

    version update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from calendarDatabase import Calendar
 
 app = Flask(__name__)
 db = Calendar()
-version = "Local V0.72"
+version = "Local V0.73"
 
 @app.route('/', methods=["GET","POST"])
 def index():
